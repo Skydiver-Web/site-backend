a05f3ac0808efb4d34bb84b60a709d411e6f4b2e
Skydiver-Web <126831858+Skydiver-Web@users.noreply.github.com>
2024-06-02 22:10:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    Jithesh: 'JigglyJithesh',
-    Ultimann: 'slavanish',
-    lukenigga: 'nigeria_',
-    construct: 'loose',
+    admin: 'password',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
